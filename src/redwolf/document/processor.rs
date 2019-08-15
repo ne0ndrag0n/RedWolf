57e83428568e3bef1f3a8bc25d6024eb28036589
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2019-08-15 17:26:41 -0400

    Update preprocessor to be able to parse toml/json structs

--- src/redwolf/document/processor.rs
+++ src/redwolf/document/processor.rs
@@ -3,6 +3,7 @@ use crate::redwolf::fdo::fdo_object::FdoObject;
 use crate::redwolf::options::CONFIG;
 use std::fs;
 use failure::Error;
+use regex::Regex;
 
 fn include_document( document_path: &str, template_params: Option< serde_json::Value > ) -> Result< String, Error > {
     let mut document = Document::load( document_path )?;
@@ -30,15 +31,23 @@ fn get_directory_list( path: &str, fragment_path: &str ) -> Result< String, Erro
  * Send the "inner text" of a processing directive
  */
 pub fn select_preprocessor( text: &str ) -> Result< String, Error > {
-    let mut tokens = text.trim().split_whitespace();
+    lazy_static! {
+        static ref PARSE_REGEX: Regex = Regex::new( r"\s+" ).expect( "bug: failed to compile static regex for select_preprocessor" );
+    };
+
+    let mut tokens = PARSE_REGEX.splitn( text.trim(), 2 );
     let first_token = tokens.next().ok_or( format_err!( "No processing directive given!" ) )?;
 
     match first_token {
-        "directory_list" => get_directory_list(
-            tokens.next().ok_or( format_err!( "Invalid first argument to processing directive 'directory_list'" ) )?,
-            tokens.next().ok_or( format_err!( "Invalid second argument to processing directive 'directory_list'" ) )?
-        ),
+        "directory_list" => {
+            let mut tokens = tokens.next().ok_or( format_err!( "No arguments provided to processing directive 'directory_list'" ) )?.split_whitespace();
+            get_directory_list(
+                tokens.next().ok_or( format_err!( "Invalid first argument to processing directive 'directory_list'" ) )?,
+                tokens.next().ok_or( format_err!( "Invalid second argument to processing directive 'directory_list'" ) )?
+            )
+        },
         "include" => {
+            let mut tokens = tokens.next().ok_or( format_err!( "No arguments provided to processing directive 'include'" ) )?.split_whitespace();
             let first_arg = tokens.next().ok_or( format_err!( "Invalid first argument to processing directive 'include'" ) )?;
             let second_arg: Option< serde_json::Value > = match tokens.next() {
                 Some( token ) => Some( serde_json::from_str( token ).map_err( | _ | format_err!( "Malformed second argument to processing directive 'include'" ) )? ),
