479662af44460499a797365561e6ca71bc653a11
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2019-08-15 18:36:21 -0400

    Paths default to index.html

--- src/redwolf/routes.rs
+++ src/redwolf/routes.rs
@@ -3,6 +3,7 @@ use crate::redwolf::errors::ResponseFailure;
 use crate::redwolf::document;
 use actix_web::{ web, get };
 use serde::Deserialize;
+use std::path::Path;
 
 #[derive(Deserialize)]
 pub struct PathOptions {
@@ -15,6 +16,13 @@ pub fn no_params() -> &'static str {
 }
 
 #[get("/documents/{path:.*}")]
-pub fn get_document( request_options: web::Path< PathOptions > ) -> Result< Option< Document >, ResponseFailure > {
+pub fn get_document( mut request_options: web::Path< PathOptions > ) -> Result< Option< Document >, ResponseFailure > {
+    // If path does not have an extension, assume directory path and add index.html
+    let path = Path::new( &request_options.path );
+    if path.extension().is_none() {
+        request_options.path = String::from( path.join( "index.html" ).as_os_str().to_str().ok_or( ResponseFailure::GeneralFailure( "Error converting osstr".to_owned() ) )? );
+    }
+
+    info!( "path: {}", request_options.path );
     document::renderer::find_document_by_path( &request_options.path )
 }
